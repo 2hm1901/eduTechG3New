ebe6ac6766296d5c2f91dbd53ddeb14d9ee29e3a
hofang <ringhost42@gmail.com>
2026-05-27 16:29:03 +0700

    upload task1, add file, add file to folder

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+window.API_BASE_URL = "https://540rls5sxg.execute-api.ap-southeast-1.amazonaws.com"; // Paste the API Gateway Endpoint URL here after sam deploy
+window.COGNITO_USER_POOL_ID = "ap-southeast-1_39LOCIDa9";
+window.COGNITO_CLIENT_ID = "1g7erhnlrrlgh7074rff2jnith";
